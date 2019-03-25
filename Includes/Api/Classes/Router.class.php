a5ecee10c1cacb47ca2ce05e888f8e62ee85a6bc
Travis Brown <warriorrocker@gmail.com>
2019-03-24 22:21:05 -0400

    Refactor api router query method into saner more flexible parts

--- Includes/Api/Classes/Router.class.php
+++ Includes/Api/Classes/Router.class.php
@@ -14,27 +14,27 @@ class XoApiClassRouter
 
 	/**
 	 * WordPress rewrite class matching query var.
-	 * 
+	 *
 	 * @since 1.0.0
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $classQueryVar = 'xo_api_class';
 
 	/**
 	 * WordPress rewrite method matching query var.
-	 * 
+	 *
 	 * @since 1.0.0
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $methodQueryVar = 'xo_api_method';
 
 	/**
 	 * Collection of endpoints and controllers defined for the Xo API.
-	 * 
+	 *
 	 * @since 1.0.0
-	 * 
+	 *
 	 * @var string[]
 	 */
 	public $controllers = array();
@@ -43,7 +43,7 @@ class XoApiClassRouter
 		$this->Xo = $Xo;
 
 		add_action('init', array($this, 'Init'), 10, 0);
-		add_action('parse_request', array($this, 'ApiQuery'), 0, 0);
+		add_action('parse_request', array($this, 'ApiQueryByParseRequest'), 0, 0);
 	}
 
 	public function Init() {
@@ -110,7 +110,7 @@ class XoApiClassRouter
 	 *
 	 * @return void
 	 */
-	public function ApiQuery() {
+	public function ApiQueryByParseRequest() {
 		global $wp;
 
 		// Return to normal operation if the Xo API is disabled
@@ -121,41 +121,28 @@ class XoApiClassRouter
 		if (empty($wp->query_vars[$this->classQueryVar]))
 			return;
 
-		// Set the header as json output
-		header('Content-Type: application/json');
-
 		// Determine the class configured for the current endpoint
 		$endpoint = $wp->query_vars[$this->classQueryVar];
-
-		// Filter the the request class
-		$class = apply_filters('xo/api/router/controllers/get', $this->controllers[$endpoint], $endpoint);
-
-		// Return an error if the class was not defined
-		if (!$class)
-			return $this->ReturnError(__('No class defined for endpoint.', 'xo'));
-
-		// Return an error if the class does not exist or was not included
-		if (!class_exists($class))
-			return $this->ReturnError(sprintf(__('Class %s was not found.', 'xo'), $class));
 
 		// Determine the desired method within the endpoint
 		$method = ((!empty($wp->query_vars[$this->methodQueryVar])) ? ucfirst($wp->query_vars[$this->methodQueryVar]) : 'Index');
 
-		// Filter the request method
-		$method = apply_filters('xo/api/router/methods/get', $method, $class, $endpoint);
-
-		// Return an error if the method was not determined
-		if (!$method)
-			return $this->ReturnError(__('No method determined for endpoint.', 'xo'));
-
-		// Return an error if the requested method is not public
-		if (!is_callable(array($class, $method)))
-			return $this->ReturnError(sprintf(__('Method %s in class %s was not found or private.', 'xo'), $method, $class));
+		// Set the header as json output
+		header('Content-Type: application/json');
 
 		// Exit and return no data if the request method is options, likely a CORS precursor
 		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
 			exit;
 
+		$request = $this->GetRequestFromPhpAndRequest();
+
+		$response = $this->ApiQueryByEndpoint($endpoint, $method, $request);
+
+		// Return the generated response object
+		die(json_encode($response));
+	}
+
+	protected function GetRequestFromPhpAndRequest() {
 		// Decode the body of the request, expected to be a json blob
 		$request = json_decode(file_get_contents("php://input"), true);
 
@@ -166,8 +153,22 @@ class XoApiClassRouter
 		// If the body was empty use the PHP combined $_REQUEST object instead
 		$request = (((is_array($request)) && count($request)) ? $request : $_REQUEST);
 
-		// Filter the request object
-		$request = apply_filters('xo/api/router/request/get', $request, $method, $class, $endpoint);
+		return $request;
+	}
+
+	protected function ApiQueryByEndpoint($endpoint, $method, $request) {
+		if (empty($this->controllers[$endpoint]))
+			return new XoApiAbstractResponse(false, __('No controller defined for endpoint.', 'xo'));
+
+		$class = $this->controllers[$endpoint];
+
+		// Return an error if the class does not exist or was not included
+		if (!class_exists($class))
+			new XoApiAbstractResponse(false, sprintf(__('Class %s was not found.', 'xo'), $class));
+
+		// Return an error if the requested method is not public
+		if (!is_callable(array($class, $method)))
+			new XoApiAbstractResponse(false, sprintf(__('Method %s in class %s was not found or private.', 'xo'), $method, $class));
 
 		// Create a class reflection to check if method requires params
 		$reflect = new ReflectionMethod($class, $method);
@@ -175,40 +176,14 @@ class XoApiClassRouter
 		// Get the parameters of the method and check if they are optional
 		if (($params = $reflect->getParameters()) && (!$params[0]->isOptional()) && (!$request)) {
 			// Return an error if the parameters were expected by the method
-			$this->ReturnError(sprintf(__('Method %s in class %s expects %u parameters.', 'xo'), $method, $class, count($params)));
+			new XoApiAbstractResponse(false, sprintf(__('Method %s in class %s expects %u parameters.', 'xo'), $method, $class, count($params)));
 		}
 
 		// Call the requested method in a new instance of the requested class using the request object
 		$response = call_user_func(array(new $class($this->Xo), $method), $request);
 
-		// Filter the response
-		$response = apply_filters('xo/api/router/response', $response, $request, $method, $class, $endpoint);
+		$response = apply_filters('xo/api/router/response', $response, $endpoint, $method, $request);
 
-		// Return the generated response object
-		$this->ReturnResponse($response);
-	}
-
-	/**
-	 * Output a json encoded response and exit.
-	 * 
-	 * @since 1.0.0
-	 * 
-	 * @param mixed $response Response to output.
-	 */
-	protected function ReturnResponse($response) {
-		echo json_encode($response);
-		die();
-	}
-
-	/**
-	 * Return a general API error.
-	 * 
-	 * @since 1.0.0
-	 * 
-	 * @param string $message Human readable reason for the error.
-	 */
-	protected function ReturnError($message) {
-		header('HTTP/1.0 404 Not Found');
-		$this->ReturnResponse(new XoApiAbstractResponse(false, $message));
+		return $response;
 	}
 }
